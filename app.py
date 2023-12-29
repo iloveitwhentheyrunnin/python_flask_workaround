3ec9db309117aa6593d7b1c3460a1555b4059baa
Maria Gigliotti <noahdrui@gmail.com>
2023-12-29 15:20:16 +0100

    feat. templates & scripts
    Setting up templates & scripts with Bundle and asset registering

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_assets import Bundle, Environment
+
+app = Flask(__name__, static_url_path='/static')
+
+js = Bundle('home.js', 'page1.js', output='gen/main.js')
+assets = Environment(app)
+assets.register('main_js', js)
 
 @app.route("/")
 def home():
-    return "Hello my advens practise app!"
+    return render_template('index.html')
---
+++ static/gen/main.js
@@ -0,0 +1,1 @@
+alert('dont worry it works :)')
