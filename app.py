c30ce10860f909ea7f8ba3d1214c9aeb45bc5efc
Maria Gigliotti <noahdrui@gmail.com>
2023-12-31 11:26:39 +0100

    feat. tasks & front
    - Card style
    - Create & Display Tasks
    - Securing SQL requests with binding var

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_mysqldb import MySQL
 from flask_bcrypt import Bcrypt 
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, static_url_path='/static', template_folder='templates')
@@ -25,10 +26,28 @@ bcrypt = Bcrypt(app)
 
 assets.register('main_js', js)
 
+# a little function to retrieve tasks 
+def get_tasks_from_database():
+    user_identifier = session['user_id']
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id, user_id, task_desc, is_done FROM tbl_tasks WHERE is_done = false and user_id = %s", (user_identifier,))
+    tasks_data = cur.fetchall()
+    print(tasks_data)
+    cur.close()
+            
+    tasks = []
+    
+    for task_data in tasks_data:
+        task = Task(user_id=task_data[1], task_desc=task_data[2], is_done=task_data[3])
+        tasks.append(task)
+    
+    return tasks
+
 @app.route("/")
 def home():
     if 'username' in session:
-        return render_template('index.html', username=session['username'])
+        tasks = get_tasks_from_database()
+        return render_template('index.html', username=session['username'], tasks = tasks)
     else:
         return render_template('index.html')
 
@@ -39,13 +58,15 @@ def login():
         pwd = request.form['password']
         
         cur = mysql.connection.cursor()
-        cur.execute(f"select username, password from tbl_users where username = '{username}'")
+        # a little binding to prevent from injections
+        cur.execute(f"select username, password, id from tbl_users where username = %s",({username}))
         user = cur.fetchone()
         cur.close()
 
         #check the pass hash
         if user and check_password_hash(user[1], pwd):
             session['username'] = user[0]
+            session['user_id'] = user[2]
             return redirect(url_for('home'))
         else:
             return render_template('login.html', error='Invalid credentials.')
@@ -71,13 +92,43 @@ def register():
         formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
 
         cur = mysql.connection.cursor()
-        cur.execute(f"insert into tbl_users (username, password, join_date) values ('{username}', '{hashed_password}', '{formatted_date}')")
+        #same here! a great request safe
+        cur.execute("INSERT INTO tbl_users (username, password, join_date) VALUES (%s, %s, %s)", (username, hashed_password, formatted_date))
         mysql.connection.commit()
         cur.close()
         
         return redirect(url_for('login'))
     
     return render_template('register.html')
+
+# Make it work with a class and a model just to flex a bit and try something different :p
+class Task:
+    def __init__(self, user_id, task_desc, is_done=False):
+        self.user_id = user_id
+        self.task_desc = task_desc
+        self.is_done = is_done
+
+    def write_in_db(self):
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO tbl_tasks (user_id, task_desc, is_done) VALUES (%s, %s, %s)",
+                    (self.user_id, self.task_desc, self.is_done))
+        mysql.connection.commit()
+        cur.close()
+
+@app.route('/create_task', methods=['POST'])
+def create_task():
+    if request.method == 'POST':
+        user_id = session['user_id']
+        task_desc = request.form.get('task_desc')
+        # Check if user_id is valid 
+        if not user_id or not task_desc:
+            return redirect(url_for('home', error='Invalid input'))
         
+        new_task = Task(user_id=user_id, task_desc=task_desc)
+
+        new_task.write_in_db()
+        
+        return redirect(url_for('home', success='Task Added'))
+    
 if __name__ == '__main__':
     app.run(debug=True,  use_reloader=False) # avoid double compilation
